0ef38044eb7902aab0b859b772c7370d4511ab3f
Dmitrii <rakhmanin.dm@gmail.com>
2023-06-13 23:20:50 +0300

    ref: translate messages

--- src/components/PopupContent/SuccessMessage.tsx
+++ src/components/PopupContent/SuccessMessage.tsx
@@ -1,15 +1,19 @@
 import { FC } from 'react';
+import { useTranslation } from 'react-i18next';
 import Text, { TextTag, TextType } from '../Text/Text';
 
-const SuccessMessage: FC = () => (
-  <>
-    <Text Tag={TextTag.h3} type={TextType.popupTitle}>
-      Сообщение отправлено
-    </Text>
-    <Text type={TextType.popupContent}>
-      Благодарим за обращение. Мы свяжемся с Вами в ближайщее время.
-    </Text>
-  </>
-);
+const SuccessMessage: FC = () => {
+  const { t } = useTranslation('translation', {
+    keyPrefix: 'messages.success',
+  });
+  return (
+    <>
+      <Text Tag={TextTag.h3} type={TextType.popupTitle}>
+        {t('title')}
+      </Text>
+      <Text type={TextType.popupContent}>{t('content')}</Text>
+    </>
+  );
+};
 
 export default SuccessMessage;
