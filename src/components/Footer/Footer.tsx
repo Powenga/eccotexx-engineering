c473738f21cfb493f2acb9f4bd930da322793389
Dmitrii <rakhmanin.dm@gmail.com>
2023-06-13 23:14:59 +0300

    ref: footer translation

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,6 +1,7 @@
 import { FC } from 'react';
 import cn from 'classnames';
 import block from 'bem-css-modules';
+import { useTranslation } from 'react-i18next';
 import Text, { TextType } from '../Text/Text';
 import styles from './Footer.module.css';
 import Navigation from '../Navigation/Navigation';
@@ -13,47 +14,35 @@ type Props = {
   className?: string;
 };
 
-const Footer: FC<Props> = ({ onPolicyClick, className }) => (
-  <footer className={cn(b(), className)}>
-    <div className={b('decoration')} />
-    <div className={b('decoration')} />
-    <div className={b('decoration')} />
-    <div className={b('container')}>
-      <Navigation className={b('menu')} />
-      <div className={b('info')}>
-        <Text type={TextType.footerInfo}>
-          ООО &quot;ЭКОТЕКС ИНЖИНИРИНГ&quot;
-        </Text>
-        <Text type={TextType.footerInfo}>
-          Юридический адрес: 399260, Липецкая область, Хлевенский район, с.п.
-          Хлевенский сельсовет, с. Хлевное, ул. Дорожная д. 1, оф. 2
-        </Text>
-        <Text type={TextType.footerInfo}>ИНН: 4817006399</Text>
-        <Text type={TextType.footerInfo}>КПП: 481701001</Text>
-        <Text type={TextType.footerInfo}>ОГРН: 1214800010776</Text>
-        <Text type={TextType.footerInfo}>ОКПО: 48182818</Text>
-        <Text type={TextType.footerInfo}>ОКВЭД: 72.19</Text>
-        <Text type={TextType.footerInfo}>
-          Банк: Липецкое отделение № 8593 ПАО СБЕРБАНКА РОССИИ г. Липецка
-        </Text>
-        <Text type={TextType.footerInfo}>к/с: 30101810800000000604</Text>
-        <Text type={TextType.footerInfo}>
-          р/с (рубль): 40702810535000003644
-        </Text>
-        <Text type={TextType.footerInfo}>БИК: 044206604</Text>
+const Footer: FC<Props> = ({ onPolicyClick, className }) => {
+  const { t } = useTranslation('translation', { keyPrefix: 'footer' });
+  return (
+    <footer className={cn(b(), className)}>
+      <div className={b('decoration')} />
+      <div className={b('decoration')} />
+      <div className={b('decoration')} />
+      <div className={b('container')}>
+        <Navigation className={b('menu')} />
+        <div className={b('info')}>
+          {t('paragraphs', { returnObjects: true }).map((element) => (
+            <Text key={element} type={TextType.footerInfo}>
+              {element}
+            </Text>
+          ))}
+        </div>
+        <button type="button" className={b('button')} onClick={onPolicyClick}>
+          <Text type={TextType.footerPolicy}>{t('policy')}</Text>
+        </button>
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://navigator.sk.ru/orn/1124216"
+        >
+          <img src={SkolkovoPath} alt={t('skolkovoAlt')} />
+        </a>
       </div>
-      <button type="button" className={b('button')} onClick={onPolicyClick}>
-        <Text type={TextType.footerPolicy}>Политика конфеденциальности</Text>
-      </button>
-      <a
-        target="_blank"
-        rel="noopener noreferrer"
-        href="https://navigator.sk.ru/orn/1124216"
-      >
-        <img src={SkolkovoPath} alt="Экотекс инжиниринг - участник Сколково" />
-      </a>
-    </div>
-  </footer>
-);
+    </footer>
+  );
+};
 
 export default Footer;
